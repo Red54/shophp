13df1ef137cede876ad94cbbee2f76c33b8845a8
谢致邦 (XIE Zhibang) <Yeking@Red54.com>
2016-06-30 13:38:21 +0800

    0.08

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -19,7 +19,7 @@ class Admin extends CI_Controller
     {
         $data['title'] = ' 管理员信息';
         $data['admin'] = $this->admin_model->get();
-        $this->load->view('admin/index', $data);
+        $this->load->view('admin/admin/index', $data);
     }
 
     public function add()
@@ -34,7 +34,7 @@ class Admin extends CI_Controller
         $this->form_validation->set_rules('status', '状态', 'trim|required');
         if ($this->form_validation->run() === false) {
             $data['title'] = ' 管理员信息 新建管理员';
-            $this->load->view('admin/add', $data);
+            $this->load->view('admin/admin/add', $data);
         } else {
             $this->admin_model->add();
             redirect('admin/admin');
@@ -52,7 +52,7 @@ class Admin extends CI_Controller
             $data['title'] = ' 管理员信息 编辑资料';
             $data['id'] = $id;
             $data['a'] = $this->admin_model->get($id);
-            $this->load->view('admin/edit', $data);
+            $this->load->view('admin/admin/edit', $data);
         } else {
             $this->admin_model->edit($id);
             redirect('admin/admin');
@@ -67,7 +67,7 @@ class Admin extends CI_Controller
         if ($this->form_validation->run() === false) {
             $data['title'] = ' 管理员信息 修改密码';
             $data['id'] = $id;
-            $this->load->view('admin/passwd', $data);
+            $this->load->view('admin/admin/passwd', $data);
         } else {
             $this->admin_model->passwd($id);
             redirect('admin/admin');
