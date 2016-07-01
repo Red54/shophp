e1783272484d04d5de655d0869fa3e59479747b4
谢致邦 (XIE Zhibang) <Yeking@Red54.com>
2016-07-02 03:25:56 +0800

    0.6

--- shophp-2.sql
+++ shophp-2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 01, 2016 at 12:56 PM
--- Server version: 10.0.25-MariaDB-1
--- PHP Version: 7.0.5-2+b1
+-- Generation Time: Jul 01, 2016 at 07:25 PM
+-- Server version: 10.1.14-MariaDB
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -80,7 +80,7 @@ CREATE TABLE `article` (
   `title` varchar(200) NOT NULL COMMENT '文章标题',
   `abstract` text NOT NULL COMMENT '摘要',
   `content` text NOT NULL COMMENT '内容',
-  `views` int(11) NOT NULL COMMENT '访问量',
+  `views` int(11) NOT NULL DEFAULT '0' COMMENT '访问量',
   `pubtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
   `status` int(11) NOT NULL COMMENT '状态'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章信息表';
@@ -136,7 +136,7 @@ CREATE TABLE `goods` (
   `sprice` float NOT NULL COMMENT '商城价',
   `sales` int(11) NOT NULL COMMENT '销售量',
   `stocks` int(11) NOT NULL COMMENT '库存量',
-  `views` int(11) NOT NULL COMMENT '访问量',
+  `views` int(11) NOT NULL DEFAULT '0' COMMENT '访问量',
   `pubtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
   `status` int(11) NOT NULL COMMENT '状态'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品信息表';
@@ -167,8 +167,8 @@ CREATE TABLE `member` (
   `mail` varchar(50) NOT NULL COMMENT '邮箱',
   `address` varchar(200) NOT NULL COMMENT '联系地址',
   `pcode` varchar(20) NOT NULL COMMENT '邮政编码',
-  `total` float NOT NULL COMMENT '消费总额',
-  `balance` float NOT NULL COMMENT '余额',
+  `total` float NOT NULL DEFAULT '0' COMMENT '消费总额',
+  `balance` float NOT NULL DEFAULT '0' COMMENT '余额',
   `regdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册日期',
   `status` int(11) NOT NULL COMMENT '状态'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='会员信息表';
@@ -178,7 +178,8 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`id`, `name`, `pass`, `passq`, `passa`, `tel`, `qq`, `mail`, `address`, `pcode`, `total`, `balance`, `regdate`, `status`) VALUES
-(1, 'test', '9fcedc4dd6507a6d29709212abc87805f5ac2e02', '1+1=?', 'da4b9237bacccdf19c0760cab7aec4a8359010b0', '12345678902', '1234567890', 'test@test.com', '海南省三沙市曾母暗沙', '573199', 0, 0, '2016-06-30 20:29:05', 1);
+(1, 'test', '9fcedc4dd6507a6d29709212abc87805f5ac2e02', '1+1=?', 'da4b9237bacccdf19c0760cab7aec4a8359010b0', '12345678902', '1234567890', 'test@test.com', '海南省三沙市曾母暗沙', '573199', 0, 0, '2016-06-30 20:29:05', 1),
+(2, 'someone', '9fcedc4dd6507a6d29709212abc87805f5ac2e02', 'some', 'fe05bcdcdc4928012781a5f1a2a77cbb5398e106', '09876543210', '987654321', 'some@one.uni', 'Matrix', '042054', 0, 0, '2016-07-01 23:22:03', 1);
 
 -- --------------------------------------------------------
 
@@ -312,7 +313,7 @@ ALTER TABLE `goods`
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '会员编号', AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '会员编号', AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `oform`
 --
